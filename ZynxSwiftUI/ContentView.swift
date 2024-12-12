fab2bf700db4951359e214109baaf508e7f9e060
Titus Logo <tituslogo@Tituss-MacBook-Air.local>
2024-12-12 12:29:54 +0700

    initial commit

---
+++ ZynxSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZynxSwiftUI
+//
+//  Created by Titus Logo on 12/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZynxSwiftUI/ZynxSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZynxSwiftUIApp.swift
+//  ZynxSwiftUI
+//
+//  Created by Titus Logo on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ZynxSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
